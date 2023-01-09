cf31e5307bb8704e61acd3e96e059a285814954c
christmasboy <zaxz21@naver.com>
2023-01-09 17:52:45 +0900

    20230109

--- src/main/java/com/greenart/firstproject/service/ProductService.java
+++ src/main/java/com/greenart/firstproject/service/ProductService.java
@@ -2,15 +2,10 @@ package com.greenart.firstproject.service;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.greenart.firstproject.entity.ProductInfoEntity;
-import com.greenart.firstproject.repository.MarketInfoRepository;
-import com.greenart.firstproject.repository.OptionInfoRepository;
 import com.greenart.firstproject.repository.ProductInfoRepository;
-import com.greenart.firstproject.vo.ProductVO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,10 +13,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductInfoRepository piRepo;
-    private final OptionInfoRepository oiRepo;
-    private final MarketInfoRepository miRepo;
 
-    public Page<ProductVO> getAllProducts(Pageable pageable) {
-
+    public Object findAllProducts(Pageable pageable) {
+        return piRepo.findAll();
+    }
+    public List findByType(String type, Pageable pageable) {
+        return piRepo.findByType(type, pageable);
     }
 }
