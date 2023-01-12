f28a609f14221f0c0a9d632b0a4ef9b4cc9eb135
StrongYJ <206freeguy@gmail.com>
2023-01-12 12:50:38 +0900

    20230112 옵션히스토리 추가

---
+++ src/main/java/com/greenart/firstproject/entity/OrderHistoryEntity.java
@@ -0,0 +1,47 @@
+package com.greenart.firstproject.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_history")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderHistoryEntity {
+    
+    @Column(name = "oh_seq")
+    private Long seq;
+
+    @Column(name = "oh_order_dt")
+    private LocalDateTime orderDt;
+
+    @Column(name = "oh_quantity")
+    private Integer quantity;
+
+    @Column(name = "oh_price")
+    private Integer price;
+
+    @Column(name = "oh_delivery_status")
+    private Integer deliveryStatus;
+
+    @Column(name = "oh_canceled")
+    private Boolean canceled;
+
+    @JoinColumn(name = "oh_ui_seq")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private UserEntity user;
+
+    @JoinColumn(name = "oh_oi_seq")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private OptionInfoEntity option;
+
+}
