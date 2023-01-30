09882d0b8946651a6c19a558823a10b0b43723d3
christmasboy <zaxz21@naver.com>
2023-01-30 19:21:07 +0900

    fix

--- src/main/java/com/greenart/firstproject/service/OrderHistoryService.java
+++ src/main/java/com/greenart/firstproject/service/OrderHistoryService.java
@@ -22,34 +22,22 @@ public class OrderHistoryService {
     private final UserRepository userRepo;
     private final PaymentInfoRepository payRepo;
 
-<<<<<<< HEAD
     public List<OrderHistoryVO> getOrderHistory(Long userSeq) {
         return payRepo.findByUserSeqWithFetch(userSeq).stream().map(OrderHistoryVO::new).toList();
     }
-=======
     // public List<OrderHistoryVO> getOrderHistory(Long userSeq) {
     //     return ohRepo.findByUserSeqWithFetch(userSeq).stream().map(OrderHistoryVO::new).toList();
     // }
->>>>>>> 979b7ab5dbdbac8e9e09430da38b78e30e4cdc33
     
-    // // 주문취소내역서비스 get
-    // public List<OrderHistoryVO> getOrderCanceled(Long userSeq, boolean canceled) {
-    //     return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
-    // }
+    // 주문취소내역서비스 get
+    public List<OrderHistoryVO> getOrderCanceled(Long userSeq, boolean canceled) {
+        return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
+    }
 
-    // // 주문취소내역서비스 patch
-    // public List<OrderHistoryVO> patchOrderCanceled(Long userSeq, boolean canceled) {
-    //     return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
+    // 주문취소내역서비스 patch
+    public List<OrderHistoryVO> patchOrderCanceled(Long userSeq, boolean canceled) {
+        return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
 
-    // }
+    }
     
-    // private OrderHistoryVO OrderCanceledHistoryVO(OrderHistoryEntity entity) {
-    //     return OrderHistoryVO.builder()
-    //     .productSeq(entity.getProduct().getSeq())
-    //     .optionName(entity.getName())
-    //     .quantity(entity.getQuantity())
-    //     .price(entity.getPrice())
-    //     .canceled(entity.getCanceled())
-    //     .build();
-    // }
 }
