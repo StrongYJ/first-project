5868c0fc95e846f164c6e59de5427ae2e084b37d
christmasboy <zaxz21@naver.com>
2023-01-31 11:44:38 +0900

    20230131시작

--- src/main/java/com/greenart/firstproject/service/OrderHistoryService.java
+++ src/main/java/com/greenart/firstproject/service/OrderHistoryService.java
@@ -22,22 +22,22 @@ public class OrderHistoryService {
     private final UserRepository userRepo;
     private final PaymentInfoRepository payRepo;
 
-    public List<OrderHistoryVO> getOrderHistory(Long userSeq) {
-        return payRepo.findByUserSeqWithFetch(userSeq).stream().map(OrderHistoryVO::new).toList();
-    }
     // public List<OrderHistoryVO> getOrderHistory(Long userSeq) {
-    //     return ohRepo.findByUserSeqWithFetch(userSeq).stream().map(OrderHistoryVO::new).toList();
+    //     return payRepo.findByUserSeqWithFetch(userSeq).stream().map(OrderHistoryVO::new).toList();
     // }
+    // // public List<OrderHistoryVO> getOrderHistory(Long userSeq) {
+    // //     return ohRepo.findByUserSeqWithFetch(userSeq).stream().map(OrderHistoryVO::new).toList();
+    // // }
     
-    // 주문취소내역서비스 get
-    public List<OrderHistoryVO> getOrderCanceled(Long userSeq, boolean canceled) {
-        return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
-    }
+    // // 주문취소내역서비스 get
+    // public List<OrderHistoryVO> getOrderCanceled(Long userSeq, boolean canceled) {
+    //     return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
+    // }
 
-    // 주문취소내역서비스 patch
-    public List<OrderHistoryVO> patchOrderCanceled(Long userSeq, boolean canceled) {
-        return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
+    // // 주문취소내역서비스 patch
+    // public List<OrderHistoryVO> patchOrderCanceled(Long userSeq, boolean canceled) {
+    //     return ohRepo.findByUserSeqCanceledWithFetch(userSeq,canceled).stream().map(OrderHistoryVO::new).toList();
 
-    }
+    // }
     
 }
