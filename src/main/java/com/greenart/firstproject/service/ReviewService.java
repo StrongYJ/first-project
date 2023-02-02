a39103b41e1cb03989754b82bc77a936c8789cb6
StrongYJ <206freeguy@gmail.com>
2023-02-02 18:27:13 +0900

    리뷰서비스에 Transactional 추가

--- src/main/java/com/greenart/firstproject/service/ReviewService.java
+++ src/main/java/com/greenart/firstproject/service/ReviewService.java
@@ -27,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ReviewService {
     private final ReviewRepository reviewRepo;
     private final UserRepository userRepo;
@@ -63,14 +64,15 @@ public class ReviewService {
         return new ReviewResponseVO(addReview.getSeq(), addReview.getOptionName(), addReview.getGrade(), addReview.getContent(), addReview.getRegDt().toString());
     }
 
+    @Transactional
     public ReviewUpdateVO updateReview(Long reviewSeq, ReviewUpdateVO data){
         // 리뷰 정보를 불러오고 없으면 예외 발생
         ReviewEntity updateReview = reviewRepo.findById(reviewSeq).orElseThrow();
         updateReview.setUpdateReview(data);
-        reviewRepo.save(updateReview);
         return data;
     }
 
+    @Transactional
     public void delete(Long seq){
         reviewRepo.delete(reviewRepo.findById(seq).orElseThrow());
     }
