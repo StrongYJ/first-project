0e9fbaf3a785378b9065834c8dda8cdf84effc99
yeong <addi0518@naver.com>
2023-02-02 16:03:09 +0900

    2023/02/02

--- src/main/java/com/greenart/firstproject/entity/ReviewImgEntity.java
+++ src/main/java/com/greenart/firstproject/entity/ReviewImgEntity.java
@@ -1,5 +1,7 @@
 package com.greenart.firstproject.entity;
 
+import com.greenart.firstproject.vo.review.ReviewCreateVO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,12 +11,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "review_img")
+@NoArgsConstructor(access = AccessLevel. PROTECTED)
 public class ReviewImgEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "rimg_seq") 
@@ -27,4 +32,9 @@ public class ReviewImgEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private ReviewEntity review;
 
+    @Builder
+    public ReviewImgEntity(String imgName, ReviewEntity reviewEntity){
+        this.rimgFileName = imgName;
+        this.review = reviewEntity;
+    }
 }
---
+++ src/main/java/com/greenart/firstproject/vo/review/ReviewResponseVO.java
@@ -0,0 +1,5 @@
+package com.greenart.firstproject.vo.review;
+
+public record ReviewResponseVO(Long productSeq, String optionName, Double grade, String content, String regDt) {
+    
+}
